7d05ef3ace8130f1e04e8c986384315a96e3182c
Terrytang <terrytangabc@outlook.com>
2018-09-23 00:15:58 +0800

    add audio files and implementation

--- lib/pages/Scripts.dart
+++ lib/pages/Scripts.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
 import '../constants/Constants.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 // 假名列表页面
 class ScriptsPage extends StatelessWidget {
-  List<Widget> makeKanaList(Map<String, String> hiragana, Map<String, String> katakan) {
+  final AudioCache player = new AudioCache(prefix: 'audios/');
+  List<Widget> makeKanaList(Map<String, String> hiragana, Map<String, String> katakana) {
     var kanaList = <Widget>[];
     Widget placeholder = Container(
-      alignment: Alignment.center,
-    );
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black12, width: 0.5)
+    ),);
 
     hiragana.forEach((String key, String value) {
       kanaList.add(
         InkWell(
           onTap: () {
-            print(value);
+            player.play(key + '.mp3');
           },
           child: Container(
-            alignment: Alignment.center,
-            child: Text(
-              value + ' | ' + katakan[key],
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 18.0,
-              ),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black12, width: 0.5)
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  value + ' | ' + katakana[key],
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 18.0,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 6.0),
+                  child: Text(key),
+                ),
+              ],
             ),
           ),
         )
@@ -39,40 +53,49 @@ class ScriptsPage extends StatelessWidget {
     return kanaList;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          child: Row(
-            children: [
-              Expanded(
-                child: GridView.count(
-                  padding: const EdgeInsets.all(10.0),
-                  crossAxisSpacing: 10.0,
-                  crossAxisCount: 5,
-                  children: makeKanaList(Constants.hiraganaBasic, Constants.katakanaBasic),
-                ),
-              )
-            ]
+  SliverFixedExtentList makeKanaTitle(String titleStr) {
+    var title = <Widget>[
+      Container(
+        alignment: Alignment.center,
+        child: Text(
+          titleStr,
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        Container(
-          child: Row(
-            children: [
-              Expanded(
-                child: GridView.count(
-                  primary: false,
-                  padding: const EdgeInsets.all(10.0),
-                  crossAxisSpacing: 10.0,
-                  crossAxisCount: 5,
-                  children: makeKanaList(Constants.hiraganaVoiced, Constants.katakanaVoiced),
-                ),
-              ),
-            ],
-          )
+      )
+    ];
+
+    return new SliverFixedExtentList(
+        delegate: new SliverChildListDelegate(title),
+        itemExtent: 48.0
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        makeKanaTitle('Basic'),
+        new SliverGrid.count(
+          crossAxisCount: 5,
+          childAspectRatio: 1.2,
+          children: makeKanaList(Constants.hiraganaBasic, Constants.katakanaBasic),
         ),
-      ]
+        makeKanaTitle('Voiced'),
+        new SliverGrid.count(
+          crossAxisCount: 5,
+          childAspectRatio: 1.2,
+          children: makeKanaList(Constants.hiraganaVoiced, Constants.katakanaVoiced),
+        ),
+        makeKanaTitle('Combo'),
+        new SliverGrid.count(
+          crossAxisCount: 3,
+          childAspectRatio: 1.8,
+          children: makeKanaList(Constants.hiraganaCombo, Constants.katakanaCombo),
+        ),
+      ],
     );
   }
 }
