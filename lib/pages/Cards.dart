e726db27f311830c6072b5b230fda3d526eb5339
Terry-PC\Terry <terrytang@lexinfintech.com>
2018-09-26 19:56:07 +0800

    create cards page

---
+++ lib/pages/Cards.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../constants/Constants.dart';
+
+class Cards extends StatelessWidget {
+  List<Widget> makeItems () {
+
+  }
+
+  @override
+  Widget build (BuildContext context) {
+    return new Scaffold(
+      body: new Text('cards')
+    );
+  }
+}
--- lib/pages/Quizzes.dart
+++ lib/pages/Quizzes.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
+import './Cards.dart';
 
 // 假名列表页面
 class QuizzesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("QuizzesPage"),
+    return Column(
+      children: <Widget>[
+        Card(
+          color: Colors.orangeAccent,
+          margin: const EdgeInsets.all(16.0),
+          child: ListTileTheme(
+            textColor: Colors.white,
+            iconColor: Colors.white,
+            contentPadding: const EdgeInsets.only(left: 26.0),
+            child: ListTile(
+              leading: const Icon(Icons.swap_horizontal_circle),
+              title: const Text('Cards'),
+              subtitle: const Text('Improve Your Memory of Kana'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Cards()));
+              },
+            ),
+          )
+        ),
+        Card(
+          color: Colors.lightGreen,
+          margin: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+          child: ListTileTheme(
+            textColor: Colors.white,
+            iconColor: Colors.white,
+            contentPadding: const EdgeInsets.only(left: 26.0),
+            child: ListTile(
+              leading: const Icon(Icons.event_note),
+              title: const Text('Dictations'),
+              subtitle: const Text('Practice Your Listening and Writing'),
+              onTap: () {
+                print(this);
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
